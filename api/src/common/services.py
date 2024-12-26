083b16755f2a2a855ebe6510c5e22327f992eb7e
Fausto <faustocamp545@gmail.com>
2024-12-26 17:00:53 -0500

    add common services

---
+++ api/src/common/services.py
@@ -0,0 +1,45 @@
+from abc import ABC, abstractmethod
+from .repository import AbstractRepository
+
+
+class AbstractService(ABC):
+    @abstractmethod
+    def create(self, data: dict):
+        pass
+
+    @abstractmethod
+    def get_by_id(self, id):
+        pass
+
+    @abstractmethod
+    def get_all(self):
+        pass
+
+    @abstractmethod
+    def update(self, id, data: dict):
+        pass
+
+    @abstractmethod
+    def delete(self, id):
+        pass
+
+
+class Service(AbstractService):
+    def __init__(self, repository: AbstractRepository):
+        self.repository = repository
+
+    def create(self, data: dict):
+        instance = self.repository.model(**data)
+        return self.repository.add(instance)
+
+    def get_by_id(self, id):
+        return self.repository.find_by_id(id)
+
+    def get_all(self):
+        return self.repository.find_all()
+
+    def update(self, id, data: dict):
+        return self.repository.update(data)
+
+    def delete(self, id):
+        return self.repository.delete_by_id(id)
