71661d99c12bdef4e4d9d16f7ad53ec245938f1f
Fausto <faustocamp545@gmail.com>
2024-12-28 12:38:27 -0500

    replace table for an model in association between invoice and product

--- api/src/invoicing/models.py
+++ api/src/invoicing/models.py
@@ -14,7 +14,7 @@ class PaymentType(db.Model):
 
 
 # association table
-invoice_items = db.Table(
+""" invoice_items = db.Table(
     "invoice_items",
     db.Model.metadata,
     db.Column("invoice_id", Integer, ForeignKey("invoices.id")),
@@ -23,24 +23,45 @@ invoice_items = db.Table(
     db.Column("price", Float, nullable=False),
     db.Column("subtotal", Float, nullable=False),
 )
+ """
+class InvoiceItem(db.Model):
+    __tablename__ = "invoice_items"
+
+    # FKs
+    invoice_id: Mapped[int] = mapped_column(
+        Integer, ForeignKey("invoices.id"), nullable=False
+    )
+    product_id: Mapped[int] = mapped_column(
+        Integer, ForeignKey("products.id"), nullable=False
+    )
+
+    quantity: Mapped[int] = mapped_column(Integer, nullable=False)
+    price: Mapped[float] = mapped_column(Float(precision=2), nullable=False)
+    subtotal: Mapped[float] = mapped_column(Float(precision=2), nullable=False)
+
+    # relationships
+    invoice = relationship("Invoice", back_populates="items")
+    product = relationship("Product", back_populates="invoices")
 
 
 class Invoice(db.Model):
     __tablename__ = "invoices"
-    ruc: Mapped[str]
+    ruc: Mapped[str] = mapped_column(String(13), nullable=False)
     created_at: Mapped[datetime] = mapped_column(
         default=lambda: datetime.now(timezone.utc)
     )
-    discount: Mapped[float]
-    total: Mapped[float]
+    discount: Mapped[float] = mapped_column(
+        Float(precision=2), nullable=False, default=0.0
+    )
+    total: Mapped[float] = mapped_column(Float(precision=2), nullable=False)
 
     # FKs
-    person_id: Mapped[int] = mapped_column(ForeignKey("people.id"))
-    payment_type_id: Mapped[int] = mapped_column(ForeignKey("payment_types.id"))
+    person_id: Mapped[int] = mapped_column(ForeignKey("people.id"), nullable=False)
+    payment_type_id: Mapped[int] = mapped_column(
+        ForeignKey("payment_types.id"), nullable=False
+    )
 
     # relationships
     person = relationship("Person", back_populates="invoices")
     payment_type = relationship("PaymentType", back_populates="invoices")
-
-    # products = relationship("Product", secondary=invoice_items, back_populates="invoices")
-    products = relationship("Product", secondary=invoice_items)
+    items = relationship("InvoiceItem", back_populates="invoice")
